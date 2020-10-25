7393fa4c29d3d1820f9fa307f82ba010782a41ad
Sofiia Yarmolenko <sophiayarmolenko@gmail.com>
2020-10-25 10:20:03 +0200

    Add saving, opening and deleting row and col

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,14 @@
 package sample;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -11,6 +17,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class Controller
 {
@@ -57,11 +65,48 @@ public class Controller
     private Button closeButton;
 
     @FXML
+    public AnchorPane saveOpenAnchorPane;
+
+    @FXML
+    private TextField saveOpenFileTextField;
+
+    @FXML
+    private Button openFileDialogButton;
+
+    @FXML
+    private Button saveButton;
+
+    @FXML
+    private Button openButton;
+
+    @FXML
+    private Button doneButton;
+
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Button removeColButton;
+
+    @FXML
+    private Button removeRowButton;
+
+    @FXML
     void initialize()
     {
         //Editable
         mainTableView.setEditable( true );
+        saveOpenAnchorPane.setDisable(true);
+        saveOpenAnchorPane.setOpacity(0);
+        setDeleteColButton();
         setStartColumns();
+        setDeleteRowButton();
+        cancelButton.setOnAction( actionEvent ->
+        {
+            saveOpenAnchorPane.setDisable(true);
+            saveOpenAnchorPane.setOpacity(0);
+            saveOpenFileTextField.setText("");
+        });
 
         showInstructionButton.setOnAction( actionEvent ->
         {
@@ -76,34 +121,320 @@ public class Controller
 
         addColButton.setOnAction ( actionEvent ->
         {
-            lastColIndex = generateNextString(lastColIndex);
-            for( int i = 0; i <= lastRowIndex; i++ )
-            {
-                if ( i == 0 )
-                {
-                    addNewColumn( lastColIndex );
-                }
-                Cell cell = new Cell( lastColIndex, "", lastColIndex + i );
-                table.get(i).put( lastColIndex, cell );
-            }
+            addCol();
         }
         );
         addRowButton.setOnAction( actionEvent ->
         {
-            lastRowIndex++;
-            HashMap<String, Cell> map = new HashMap<String, Cell>();
-            String colIndex = "A";
-            while ( !colIndex.equals(lastColIndex) )
-            {
-                Cell cell = new Cell( colIndex, "", colIndex + lastRowIndex );
-                map.put( colIndex, cell );
-                colIndex = generateNextString( colIndex );
-            }
-            Cell cell = new Cell( colIndex, "", colIndex + lastRowIndex );
-            map.put( colIndex, cell );
-            table.add(map);
+            addRow();
         });
         mainTableView.setItems( table );
+        Stage stage = new Stage();
+        save(stage);
+        Stage stage2 = new Stage();
+        open(stage2);
+    }
+
+    private void addCol()
+    {
+        lastColIndex = generateNextString(lastColIndex);
+        if(lastColIndex.equals("B"))
+        {
+            removeColButton.setVisible(true);
+            removeColButton.setOpacity(1);
+        }
+        for( int i = 0; i <= lastRowIndex; i++ )
+        {
+            if ( i == 0 )
+            {
+                addNewColumn( lastColIndex );
+            }
+            Cell cell = new Cell( lastColIndex, "", lastColIndex + i );
+            table.get(i).put( lastColIndex, cell );
+        }
+        resetTable();
+    }
+    private void addRow()
+    {
+        lastRowIndex++;
+        if( lastRowIndex == 1 )
+        {
+            removeRowButton.setVisible(true);
+            removeRowButton.setOpacity(1);
+        }
+        HashMap<String, Cell> map = new HashMap<String, Cell>();
+        String colIndex = "A";
+        while ( !colIndex.equals(lastColIndex) )
+        {
+            Cell cell = new Cell( colIndex, "", colIndex + lastRowIndex );
+            map.put( colIndex, cell );
+            colIndex = generateNextString( colIndex );
+        }
+        Cell cell = new Cell( colIndex, "", colIndex + lastRowIndex );
+        map.put( colIndex, cell );
+        table.add(map);
+        resetTable();
+    }
+    private void setDeleteColButton ()
+    {
+        removeColButton.setOnAction( actionEvent ->
+        {
+            deleteLastCol();
+        });
+    }
+    private void deleteLastCol()
+    {
+        deleteCol(lastColIndex);
+        lastColIndex = generatePreviousString(lastColIndex);
+        if(lastColIndex.equals("A"))
+        {
+            removeColButton.setVisible(false);
+            removeColButton.setOpacity(0);
+        }
+    }
+    private void deleteCol (String colName)
+    {
+        try
+        {
+            for( HashMap<String, Cell> map : table )
+            {
+                setError(map.get(colName));
+                map.remove(colName);
+            }
+            for( TableColumn column : mainTableView.getColumns())
+            {
+                if( column.getText().equals(colName) )
+                {
+                    mainTableView.getColumns().remove(column);
+                    break;
+                }
+            }
+        }
+        catch (Exception e){}
+    }
+
+    private void setError(Cell cell)
+    {
+        if(cell.listOfCellsWhereAppears != null)
+        {
+            for(Cell cell3 : cell.listOfCellsWhereAppears)
+            {
+                cell3.setStringValue("Error");
+                setError(cell3);
+            }
+        }
+
+    }
+
+    private void setDeleteRowButton ()
+    {
+        removeRowButton.setOnAction( actionEvent ->
+        {
+           deleteLastRow();
+        });
+    }
+    private void deleteLastRow()
+    {
+        deleteRow(lastRowIndex);
+        lastRowIndex--;
+        if(lastRowIndex == 0)
+        {
+            removeRowButton.setVisible(false);
+            removeRowButton.setOpacity(0);
+        }
+    }
+    private void deleteRow (int rowName)
+    {
+        try
+        {
+            for(Cell cell : table.get(rowName).values())
+            {
+                setError(cell);
+            }
+            table.remove(rowName);
+            mainTableView.refresh();
+        }
+        catch (Exception e){}
+    }
+
+    private void save(Stage primaryStage)
+    {
+        saveButton.setOnAction(actionEvent ->
+        {
+            AtomicBoolean fromFileDialog = new AtomicBoolean(false);
+            saveOpenAnchorPane.setDisable( false );
+            saveOpenAnchorPane.setOpacity(0.9);
+            saveOpenFileTextField.setPromptText("Введіть назву файла для збереження");
+
+            openFileDialogButton.setOnAction( actionEvent1 ->
+            {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open Resource File");
+                File file = fileChooser.showOpenDialog(primaryStage);
+                saveOpenFileTextField.setText( file.getName() );
+                fromFileDialog.set(true);
+                saveFromFile(file);
+            });
+
+            if( !fromFileDialog.get() )
+            {
+                saveFromFileName();
+            }
+        });
+    }
+
+    private void saveFromFile(File file)
+    {
+        doneButton.setOnAction(actionEvent2 ->
+        {
+            try
+            {
+                FileWriter fileWriter = new FileWriter( file );
+                fileWriter.write(lastRowIndex + " " + lastColIndex);
+                fileWriter.append('\n');
+                for ( HashMap<String, Cell> map : Controller.table )
+                {
+                    for( Cell cell : map.values() )
+                    {
+                        if(!cell.getStringValue().equals(""))
+                        {
+                            fileWriter.write(cell.getCellCol() + " "
+                                                + cell.getCellName() + " "
+                                                + cell.getStringValue() + " "
+                                                + cell.getFormula());
+                            fileWriter.append('\n');
+                        }
+                    }
+                }
+                fileWriter.close();
+                saveOpenAnchorPane.setDisable( true );
+                saveOpenAnchorPane.setOpacity(0);
+            }
+            catch (IOException e)
+            {
+                saveOpenFileTextField.setPromptText("Назва файлу не коректна. Введіть правильну назву файла");
+            }
+
+        });
+    }
+
+    private void saveFromFileName()
+    {
+        String fileName = saveOpenFileTextField.getText();
+        File file = new File(fileName);
+        saveFromFile(file);
+    }
+
+    private void open(Stage stage)
+    {
+        openButton.setOnAction(actionEvent ->
+        {
+            AtomicBoolean fromFileDialog = new AtomicBoolean(false);
+            saveOpenAnchorPane.setDisable( false );
+            saveOpenAnchorPane.setOpacity(0.9);
+            saveOpenFileTextField.setPromptText("Введіть назву файла для відкриття");
+
+            openFileDialogButton.setOnAction( actionEvent1 ->
+            {
+                FileChooser fileChooser = new FileChooser();
+                fileChooser.setTitle("Open Resource File");
+                File file = fileChooser.showOpenDialog(stage);
+                saveOpenFileTextField.setText( file.getName() );
+                fromFileDialog.set(true);
+                openFromFile(file);
+            });
+
+            if( !fromFileDialog.get() )
+            {
+               openFromFileName();
+            }
+        });
+    }
+
+    private void openFromFile(File file)
+    {
+        doneButton.setOnAction(actionEvent2 ->
+        {
+            try
+            {
+                FileReader fileReader = new FileReader( file );
+                Scanner scanner = new Scanner(fileReader);
+                String firstLine  = scanner.nextLine();
+                int expectedRow = Integer.parseInt(firstLine.split(" ")[0]);
+                String expectedCol = firstLine.split(" ")[1];
+                setTableSizeFromFile(expectedRow, expectedCol);
+                clearTable();
+                while( scanner.hasNext() )
+                {
+                    String[] line = scanner.nextLine().split(" ");
+                    String col = line[0];
+                    int row = Integer.parseInt(line[1].replaceAll(col, ""));
+                    table.get(row).get(col).setStringValue(line[2]);
+                    table.get(row).get(col).setFormula(line[3]);
+                    mainTableView.refresh();
+                }
+                fileReader.close();
+                resetTable();
+                saveOpenAnchorPane.setDisable( true );
+                saveOpenAnchorPane.setOpacity(0);
+            }
+            catch (IOException e)
+            {
+                saveOpenFileTextField.setPromptText("Назва файлу не коректна. Введіть правильну назву файла");
+            }
+
+        });
+    }
+
+    private void clearTable()
+    {
+        for(HashMap<String, Cell> map : table)
+        {
+            for(Cell cell : map.values())
+            {
+                cell.setStringValue("");
+                cell.setFormula("");
+                cell.setDoubleValue(0);
+            }
+        }
+    }
+
+    private void openFromFileName()
+    {
+        String fileName = saveOpenFileTextField.getText();
+        File file = new File(fileName);
+        openFromFile(file);
+    }
+    private void setTableSizeFromFile(int expectedRow, String expectedCol)
+    {
+        if(lastRowIndex < expectedRow)
+        {
+            while( lastRowIndex != expectedRow )
+            {
+                addRow();
+            }
+        }
+        else if(lastRowIndex > expectedRow)
+        {
+            while( lastRowIndex != expectedRow )
+            {
+                deleteLastRow();
+            }
+        }
+        if(lastColIndex.compareTo(expectedCol) < 0)
+        {
+            while( !lastColIndex.equals(expectedCol) )
+            {
+                addCol();
+            }
+        }
+        else if( lastColIndex.compareTo(expectedCol) > 0)
+        {
+            while( !lastColIndex.equals(expectedCol) )
+            {
+                deleteLastCol();
+            }
+        }
     }
 
     private void addNewColumn(String columnName)
@@ -113,15 +444,14 @@ public class Controller
         col.setCellFactory( TextFieldTableCell.< HashMap<String, Cell>> forTableColumn() );
         col.setMinWidth(cellStartWidth);
 
-        //On Cell edit
         col.setOnEditStart( (TableColumn.CellEditEvent< HashMap<String, Cell> , String> event) ->
-        {
-            TablePosition< HashMap<String, Cell> , String> pos = event.getTablePosition();
-            int row = pos.getRow();
-            Cell cell = event.getTableView().getItems().get(row).get(columnName);
-            formulaLabel.setText(cell.getFormula());
-            formulaNameLabel.setText(cell.getCellName());
-        }
+                {
+                    TablePosition< HashMap<String, Cell> , String> pos = event.getTablePosition();
+                    int row = pos.getRow();
+                    Cell cell = event.getTableView().getItems().get(row).get(columnName);
+                    formulaLabel.setText(cell.getFormula());
+                    formulaNameLabel.setText(cell.getCellName());
+                }
         );
 
         // On Cell edit commit (for col)
@@ -152,24 +482,24 @@ public class Controller
         String colIndex = "A";
         lastRowIndex = rowNumber-1;
         for( int i = 0; i < rowNumber; i++ )
+        {
+            HashMap<String, Cell> map = new HashMap<String, Cell>();
+            colIndex = "A";
+            for( int j = 0; j < columnsNumber; j++ )
             {
-                HashMap<String, Cell> map = new HashMap<String, Cell>();
-                colIndex = "A";
-                for( int j = 0; j < columnsNumber; j++ )
+                if ( i == 0 )
                 {
-                    if ( i == 0 )
+                    addNewColumn( colIndex );
+                    if( j == columnsNumber - 1)
                     {
-                        addNewColumn( colIndex );
-                        if( j == columnsNumber - 1)
-                        {
-                            lastColIndex = colIndex;
-                        }
+                        lastColIndex = colIndex;
                     }
-                    Cell cell = new Cell( colIndex, "", colIndex + i );
-                    map.put( colIndex, cell );
-                    colIndex = generateNextString( colIndex );
                 }
-                table.add(map);
+                Cell cell = new Cell( colIndex, "", colIndex + i );
+                map.put( colIndex, cell );
+                colIndex = generateNextString( colIndex );
+            }
+            table.add(map);
 
         }
     }
@@ -191,13 +521,13 @@ public class Controller
                 break;
             }
             else
+            {
+                if( i == 0 )
                 {
-                    if( i == 0 )
-                    {
-                        for( int j = 0; j <= stringLength; j++ )
-                            nextString.append('A');
-                    }
+                    for( int j = 0; j <= stringLength; j++ )
+                        nextString.append('A');
                 }
+            }
         }
         return nextString.toString();
     }
@@ -213,5 +543,57 @@ public class Controller
             return (char) asciiPreviousLetter;
         }
         throw new IllegalArgumentException( String.valueOf(previousLetter) );
+    }
+
+    private String generatePreviousString( String nextString )
+    {
+        StringBuilder previousString = new StringBuilder();
+        int stringLength = nextString.length();
+        for ( int i = stringLength - 1; i >= 0; i-- )
+        {
+            char ch = nextString.charAt(i);
+            if( ch > 'A' )
+            {
+                previousString.append( nextString.substring(0 , i) );
+                previousString.append( generatePreviousLetter(ch) );
+                for( int j = i + 1; j < stringLength; j++ )
+                    previousString.append('Z');
+                break;
+            }
+            else
+            {
+                if( i == 0 )
+                {
+                    for( int j = 0; j < stringLength - 1; j++ )
+                        previousString.append('Z');
+                }
+            }
+        }
+        return previousString.toString();
+    }
+
+    private char generatePreviousLetter( char nextLetter )
+    {
+        if ('A' < nextLetter && nextLetter <= 'Z')
+        {
+            int asciiPreviousLetter = (int) nextLetter;
+            asciiPreviousLetter--;
+            return (char) asciiPreviousLetter;
+        }
+        throw new IllegalArgumentException( String.valueOf(nextLetter) );
+    }
+
+    private void resetTable()
+    {
+        for( HashMap<String, Cell> map : table )
+        {
+            for(Cell cell : map.values() )
+            {
+                if( !cell.getStringValue().equals("") )
+                {
+                    cell.calculateFormulaInCell();
+                }
+            }
+        }
     }
 }
